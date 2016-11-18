10cc5effef79e1e0431c58218b76570e05a3fc4c
Jussi Paananen <jussi.paananen@uef.fi>
2016-11-18 15:51:34 +0200

    Added simulation function.

---
+++ src/functions.R
@@ -0,0 +1,23 @@
+#' Simulate multivariate data based on given reference data
+#'
+#' Uses \code{mvrnorm} function to simulate data matrix of given dimensions, 
+#' using provided data as reference for the multivariate normal distributions.
+#'
+#' @param data matrix to be used as reference
+#' @param nrow the desired number of rows.
+#' @param ncol the desired number of columns.
+#' @param ...  additional parameters to be passed to the \code{mvrnorm} function.
+#'
+#' @return data matrix with the simulated data.
+#' @export
+#'
+#' @examples
+#' 
+simulate_data <- function(data, nrow, ncol, ...) {
+  
+  require(MASS)
+  
+  simulated_data <- matrix(mvrnorm(n=nrow*ncol, mu=colMeans(reference_data), Sigma=cov(data), ...), nrow=nrow, ncol=ncol)
+
+  simulated_data
+}
---
+++ src/simulate.R
@@ -0,0 +1,6 @@
+path <- "~/Documents/impute-metabo/"
+
+reference_data <- as.matrix(read.csv(paste0(path, "data/reference_data.csv")))
+
+simulated_data <- simulate_data(data=reference_data, nrow=100, ncol=100)
+
