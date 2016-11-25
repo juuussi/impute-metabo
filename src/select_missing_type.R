1717f72ac72e4c47ad0793507445388796ad78f8
Marietta Kokla <mariekok@C1MS4JCAH3QD.local>
2016-11-25 16:23:37 +0200

    changes in select_missing_type

--- src/select_missing_type.R
+++ src/select_missing_type.R
@@ -1,23 +1,24 @@
-## create a function that simulates the three types of missing data(mcar,mnar,mar)
-## choice of different percenatges
 
 select_missing_type <- function(data,type,alphaP){
+  ## a function that simulates the three types of missing data (mcar,mnar,mar)
+  ## choice of different percentages of missingness 
+  
   N <- length(simulated_data)
   if (type == 1){
-  # mcar
+  # mcar(missingness is random,flip of a coin)
     mcar <- rbinom(N,1,prob=alphaP)
+    #simulate MCAR data
     simulation <-data*(1-mcar)+mcar*99999  
     simulation[simulation==99999]=NA                  #change 99999 to NA (R's notation for missing)
     
   }
   else if (type == 2){
-    #mar 
-    # create a depedence
-    
-
+    #mar : missing at random
+    # create a depedence (missing is dependent on the observed values)
     mean_mar <- mean(x=data)
     sd_mar  <- sd(x=data)
     cut   <- mean_mar+sd_mar*qnorm(alphaP)
+    # simulate MAR data
     simulation <-  data
     simulation[simulation<cut] <- NA
   
