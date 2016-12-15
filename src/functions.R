1c2f060fcc5c5c2e838e5052a10cfae2b93f3348
mariekok <marietta.kokla@uef.fi>
2016-12-15 17:07:26 +0200

    added RMSE_simulate function in function.R

--- src/functions.R
+++ src/functions.R
@@ -137,3 +137,20 @@ impute <- function(data, methods) {
   imputed_data
   
 }
+
+
+
+#' Title RMSE_simulated
+#' Calculates the root mean square error between simulated(reference) data and imputed data
+#' @param data1 = simulated data matrix
+#' @param data2 = data matrix with missing values
+#' @param data3 = imputed data matrix
+#'
+#' @return
+#' @export rmse
+#'
+#' @examples
+RMSE_simulated <- function(data1,data2,data3){
+  sum((data1[is.na(data2)] - data3[is.na(data2)])^2) / sum(data1[is.na(data2)]^2)
+  
+}
