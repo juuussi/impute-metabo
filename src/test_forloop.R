a6261eaf89cb70af262a3c15563ffa3bb1d88774
mariekok <marietta.kokla@uef.fi>
2016-12-29 16:34:21 +0200

    minor changes

--- src/test_forloop.R
+++ src/test_forloop.R
@@ -5,7 +5,7 @@ library(doMC)
 library(futile.logger)
 library(pcaMethods)
 #####################
-registerDoMC(cores=1)
+registerDoMC(cores=30)
 
 path <- "~/projects/impute-metabo/"
 #output_path <- '/home/users/mariekok/projects/impute-metabo/results/result.csv'
@@ -13,21 +13,22 @@ source(paste0(path,"src/functions.R"))
 flog.appender(appender.tee(paste0(path,"results/logFile.log")))
 
 flog.threshold(INFO)
-data_size <- matrix(c(50,150,200,500,600,800),nrow=3,ncol = 2)
+data_size <- matrix(c(50,150,200,500,600,1500),nrow=3,ncol = 2)
 reference_data <- as.matrix(read.csv(paste0(path, "data/reference_data.csv")))
 
 set.seed(1406)
 for (ia in 1:nrow(data_size)){
-
-  for (ja in 1:ncol(data_size)) {
-n_iterations <- 2
-n_rows <- data_size[ia,1]
-n_cols <- data_size[ja,2]
-# n_iterations <- 2
-# n_rows <-100
-# n_cols <- 50
-    miss_proportions <- c(0.01, 0.1, 0.2, 0.3)
-    imputation_methods <- c( "RF","PPCA","mean", "min")
+  
+  for (ja in 1:nrow(data_size)) {
+    n_iterations <- 3
+    
+    n_rows <- data_size[ia,1]
+    n_cols <- data_size[ja,2]
+    # n_iterations <- 2
+    # n_rows <-100
+    # n_cols <- 50
+    miss_proportions <- c(0.01, 0.1,0.3,0.4)
+    imputation_methods <- c( "min","mean","PPCA","RF")
     
     full_results <- foreach(iteration=1:n_iterations, .combine="rbind") %dopar% {
       
@@ -42,19 +43,20 @@ n_cols <- data_size[ja,2]
           flog.info(paste('iteration:', iteration, 'miss proportion:', prop, "method:", method, sep=" "))
           
           miss_data <- simulate_missingness(data=simulated_data, mcar=prop)
-          imputed_data <- impute(data=miss_data[[1]], methods=method)
+          imputed_data <- impute(data=miss_data, methods=method)
           
           
           #COMPARING RESULTS USING ROOT MEAN SQUARE ERROR
           
-          results_mse <- MSE_simulated(data1 = simulated_data,data2 = miss_data[[1]],data3 = imputed_data)
-          results_f <- cbind(data.frame(Method=method, Prop=prop, Iteration=iteration),results_mse )
+          results_rmse <- RMSE_simulated(data1 = simulated_data,data2 = miss_data,data3 = imputed_data)
+          results_f <- cbind(data.frame(Method=method, Prop=prop, Iteration=iteration),results_rmse )
           
           results_f
           
           
         }
         method_results
+        
       }  
       
       
@@ -65,9 +67,9 @@ n_cols <- data_size[ja,2]
     
     full_results
     flog.info("**** full results ****")
-    write.csv(x=full_results, file=paste0(path, "results/result_",data_size[ia,1],"_",data_size[ja,2], ".csv"), row.names=FALSE)
-
+    write.csv(x=full_results, file=paste0(path, "results/result_NeWmcar_",data_size[ia,1],"_",data_size[ja,2], ".csv"), row.names=FALSE)
+    
   }
-
-
- }
+  
+  
+}
