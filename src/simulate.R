fb4edcbdff6cb6c0b332198c32cf6ba4e7e74d97
mariekok <marietta.kokla@uef.fi>
2016-12-29 16:30:35 +0200

    added random dataset sizes

--- src/simulate.R
+++ src/simulate.R
@@ -1,11 +1,12 @@
 
+
 # libraries
 library(missForest)
 library(doMC)
 library(futile.logger)
 library(pcaMethods)
 #####################
-registerDoMC(cores=15)
+registerDoMC(cores=30)
 
 path <- "~/projects/impute-metabo/"
 #output_path <- '/home/users/mariekok/projects/impute-metabo/results/result.csv'
@@ -13,65 +14,63 @@ source(paste0(path,"src/functions.R"))
 flog.appender(appender.tee(paste0(path,"results/logFile.log")))
 
 flog.threshold(INFO)
-
-
+data_size <- matrix(c(50,150,200,500,600,1500),nrow=3,ncol = 2)
 reference_data <- as.matrix(read.csv(paste0(path, "data/reference_data.csv")))
 
 set.seed(1406)
-
-n_iterations <- 2
-n_rows <- 150
-n_cols <- 50
-
-miss_proportions <- c(0.01, 0.1, 0.2, 0.3, 0.6)
-imputation_methods <- c("RF", "PPCA","mean", "min")
-
-full_results <- foreach(iteration=1:n_iterations, .combine="rbind") %dopar% {
+for (ia in 1:nrow(data_size)){
   
-  flog.info(paste('ITERATION', iteration, sep=' '))
-  
-  simulated_data <- simulate_data(data=reference_data, nrow=n_rows, ncol=n_cols)
-  
-  proportion_results <- foreach(i=1:length(miss_proportions), .combine="rbind") %do% {
-    method_results <- foreach(j=1:length(imputation_methods), .combine="rbind") %do% {
-      prop <- miss_proportions[i]
-      method <- imputation_methods[j]
-      flog.info(paste('iteration:', iteration, 'miss proportion:', prop, "method:", method, sep=" "))
-      #flog.info(paste('miss proportion', prop, sep=' '))
-      #flog.info(paste('method', method, sep=' '))
+  for (ja in 1:nrow(data_size)) {
+    n_iterations <- 3
+    
+    n_rows <- data_size[ia,1]
+    n_cols <- data_size[ja,2]
+    # n_iterations <- 2
+    # n_rows <-100
+    # n_cols <- 50
+    miss_proportions <- c(0.01, 0.1,0.3,0.4)
+    imputation_methods <- c( "min","mean","PPCA","RF")
+    
+    full_results <- foreach(iteration=1:n_iterations, .combine="rbind") %dopar% {
+      
+      flog.info(paste('ITERATION', iteration, sep=' '))
+      
+      simulated_data <- simulate_data(data=reference_data, nrow=n_rows, ncol=n_cols)
+      
+      proportion_results <- foreach(i=1:length(miss_proportions), .combine="rbind") %do% {
+        method_results <- foreach(j=1:length(imputation_methods), .combine="rbind") %do% {
+          prop <- miss_proportions[i]
+          method <- imputation_methods[j]
+          flog.info(paste('iteration:', iteration, 'miss proportion:', prop, "method:", method, sep=" "))
+          
+          miss_data <- simulate_missingness(data=simulated_data, mcar=prop)
+          imputed_data <- impute(data=miss_data, methods=method)
+          
+          
+          #COMPARING RESULTS USING ROOT MEAN SQUARE ERROR
+          
+          results_rmse <- RMSE_simulated(data1 = simulated_data,data2 = miss_data,data3 = imputed_data)
+          results_f <- cbind(data.frame(Method=method, Prop=prop, Iteration=iteration),results_rmse )
+          
+          results_f
+          
+          
+        }
+        method_results
+        
+      }  
       
       
-      miss_data <- simulate_missingness(data=simulated_data, mnar=prop)
-      imputed_data <- impute(data=miss_data, methods=method)
-      # #####################################################################
-      # # COMPARING RESULTS USING CORRELATION
-      # #compared_results <- compare_results(simulated_data, imputed_data)
-      # flog.info("**** COMPARING ****")
-      # #compared_results <- compare_results2(simulated_data, imputed_data, missing=miss_data)
-      # flog.info("**** DONE ****")
-      # 
-      # #results <- cbind(data.frame(Method=method, Prop=prop, Iteration=iteration), compared_results)
-      # flog.info("**** CBIND DONE ****")
-      # 
-      ######################################################################
-      
-      #COMPARING RESULTS USING ROOT MEAN SQUARE ERROR
-      
-      results_rmse <- RMSE_simulated(data1 = simulated_data,data2 = miss_data,data3 = imputed_data)
-      results_f <- cbind(data.frame(Method=method, Prop=prop, Iteration=iteration),results_rmse )
-      
-      results_f
-      
+      proportion_results
       
     }
-    method_results
-  }  
-
+    
+    
+    full_results
+    flog.info("**** full results ****")
+    write.csv(x=full_results, file=paste0(path, "results/result_NeWmcar_",data_size[ia,1],"_",data_size[ja,2], ".csv"), row.names=FALSE)
+    
+  }
   
-  proportion_results
   
 }
-
-
-full_results
-write.csv(x=full_results, file=paste0(path, "results/result.csv"), row.names=FALSE)
