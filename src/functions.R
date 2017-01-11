e8d0d3e3f36f1a7ff0126d6ecb1eac6455ee7f59
Jussi Paananen <jussi.paananen@uef.fi>
2017-01-11 16:09:50 +0200

    Removed results files. Edited mnar simulation.

--- src/functions.R
+++ src/functions.R
@@ -76,43 +76,54 @@ simulate_missingness <- function(data, mcar=0, mar=0, mnar=0, mnar.type="left") 
   if(class(data) != "matrix") {
     stop("Variable data should be a matrix.")
   }
+  if ( ((mcar + mar + mnar) > 1) || ((mcar + mar + mnar) < 0)) {
+    stop("Sum of mcar, mar and mnar should be between 0 and 1.")
+  }
+  
   simulated_data <- data
   if (mcar > 0){
     mcar_distribution   = runif(nrow(data)*ncol(data), min=0, max=1)
     simulated_data = matrix(ifelse(mcar_distribution<mcar, NA, data), nrow=nrow(data), ncol=ncol(data))
   }
   if (mnar > 0) {
-      added_mnar <- 0
-      while (added_mnar < mnar) {
-        
-        # Select random variable
-        variable_index <- sample(1:ncol(simulated_data), 1)
-        
-        # What percentage of variable to set missing
+    added_mnar <- 0
+    initial_nas <- sum(colSums(is.na(simulated_data)))
+    current_nas <- initial_nas
+    simulated_data_n <- ncol(simulated_data) * nrow(simulated_data)
+    
+    while (((current_nas - initial_nas) / simulated_data_n) < mnar) {
+      
+      # Select random variable
+      variable_index <- sample(1:ncol(simulated_data), 1)
+      
+      # What percentage of variable to set missing
+      cut_percentage <- 0
+      while (cut_percentage <= 0) {
         cut_percentage <- rchisq(1, df=1) / 30
+      }
+      cut_percentage <- min(cut_percentage, 1)
+      
+      # How many values to set missing
+      cut_index <- floor(cut_percentage * nrow(simulated_data))
+      
+      sorted_variable <- sort(simulated_data[,variable_index])
+      
+      # Set values to missing
+      if (mnar.type == "right") {
+        # Corresponding cut-off point for values
+        cut_point <- sorted_variable[length(sorted_variable) - cut_index]
         
-        if (cut_percentage > 1) {
-          cut_percentage <- 1
-        }
-        while (cut_percentage == 0) {
-          cut_percentage <- rchisq(1, df=1) / 30
-          
-        }
-        
-        # How many values to set missing
-        cut_index <- floor(cut_percentage * nrow(simulated_data))
-        
-        sorted_variable <- sort(simulated_data[,variable_index])
+        simulated_data[simulated_data[,variable_index] > cut_point, variable_index] <- NA
+      } else {
         # Corresponding cut-off point for values
         cut_point <- sorted_variable[cut_index]
         
-        
-        # Set values to missing
         simulated_data[simulated_data[,variable_index] < cut_point, variable_index] <- NA
-        
-        # Counter to check how much MNAR missingness has been added to data
-        added_mnar <- added_mnar + (cut_index-1) / (nrow(simulated_data) * ncol(simulated_data))
       }
+      
+      # Counter to check how much MNAR missingness has been added to data
+      current_nas <- sum(colSums(is.na(simulated_data)))
+    }
   }
   
   simulated_data
@@ -153,14 +164,14 @@ impute <- function(data, methods) {
     imputed_data[,index] <- rf_imputed_data[,index]
   }
   
-   if ("PPCA" %in% methods) {
-     # Do cross validation with ppca for component 2:10
-     esti <- kEstimate(data, method = "ppca", evalPcs = 2:10, nruncv=1, em="nrmsep")
-     # The best result was obtained for this number of PCs:esti$bestNPcs
-     pc <- pcaMethods::pca(data,nPcs=esti$bestNPcs, method="ppca")
-     #index <- which(methods == "PPCA")
-     imputed_data <- completeObs(pc)
-     
+  if ("PPCA" %in% methods) {
+    # Do cross validation with ppca for component 2:10
+    esti <- kEstimate(data, method = "ppca", evalPcs = 2:10, nruncv=1, em="nrmsep")
+    # The best result was obtained for this number of PCs:esti$bestNPcs
+    pc <- pcaMethods::pca(data,nPcs=esti$bestNPcs, method="ppca")
+    #index <- which(methods == "PPCA")
+    imputed_data <- completeObs(pc)
+    
     
   }
   
