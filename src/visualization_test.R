1776c7e9f9b7ef4a88596a26039072f0646ab1ba
mariekok <marietta.kokla@uef.fi>
2017-06-27 16:04:43 +0300

    added summury matrix

--- src/visualization_test.R
+++ src/visualization_test.R
@@ -1,15 +1,11 @@
 
 library(doMC)
 library(ggplot2)
-# install.packages("ggplot2")
-# install.packages("resha
+
+library("reshape2")
 library("dplyr")
 library(tidyr)
-# install.packages("devtools")
-# library(devtools)
-# install_github("wilkox/ggfittext")
-# install_github("wilkox/treemapify")
-
+library(matrixStats)
 registerDoMC(cores=1)
 ####
 path <- "~/projects/impute-metabo/"
@@ -20,7 +16,7 @@ source(paste0(path,"src/functions.R"))
 #fileNames <- Sys.glob(paste0(path,"results/","results_RF_KNN_big_sample.csv"))
 
 
-fileNames <- Sys.glob(paste0(path,"results/","results_ALLMETHODS_ZERO_062017_4.csv"))
+fileNames <- Sys.glob(paste0(path,"results/","results_ALLMETHODS_ZERO_062017_5_3p.csv"))
 
   # # read data
   # 
@@ -47,24 +43,64 @@ fileNames <- Sys.glob(paste0(path,"results/","results_ALLMETHODS_ZERO_062017_4.c
 # 
 # p
 #
-results_new <- data.frame(Method=results$Method,Type = results$MissModel,Percentage= results$Miss,Size=results$Cols,Error=results$NRMSE)
-
-View(results_new)
-
-new <- results_new %>%
-  group_by(Method,Type,Percentage) %>%
-  summarise(Total_mean = mean(Error, na.rm = TRUE ))%>%
-  spread(Type,Total_mean)%>%
-  arrange(Percentage,Method)
-
-colnames(new)[3:ncol(new)] <- paste("Mean",colnames(new)[3:ncol(new)])
 
 
+results_new <- data.frame(Method=results$Method,Type = results$MissModel,Percentage= results$Miss,Size=results$Cols,Error=results$NRMSE,Time_total = results$Time_total)
+
+# View(results_new)
+# 
+# new <- results_new %>%
+#   group_by(Method,Type) %>%
+#   summarise(Total_mean = mean(Error, na.rm = TRUE) ,Total_sd = sd(Error,na.rm = TRUE))%>%
+#   gather(Statistic, Value, c(Total_mean,Total_sd))%>%
+#   spread(Statistic, Value)%>% # it spreads the error through the columns
+#   arrange(Percentage,Method)
+# 
+# colnames(new)[3:ncol(new)] <- paste("Mean_Error",colnames(new)[3:ncol(new)])
+# 
+# results_new <- data.frame(Method=results$Method,Type = results$MissModel,Percentage= results$Miss,Size=results$Cols,Error=results$NRMSE)
 
 
+new2 <- results_new %>%
+  group_by(Method,Percentage,Type) %>%
+  summarise(Total_mean = mean(Error,na.rm=TRUE))%>%
+  ungroup() %>%
+  spread(Type,Total_mean)%>% # it spreads the error through the columns
+  arrange(Method)
+
+colnames(new2)[3:ncol(new2)] <- paste("Mean_Error",colnames(new2)[3:ncol(new2)], sep = "_" )
+# calculate the total sum error of the columns with the mean
+new2$Total_Error <- rowSums(new2 %>% select(starts_with("Mean_")))
+
+# pick the columns with the name mean
+mean_col <- colnames(new2)[startsWith(colnames(new2), "Mean")]
+
+new2$Total_MEAN_Error <- apply(new2[,mean_col],1, mean , na.rm = TRUE) # same result as before.
+
+new2$Total_SD_Error <- apply(new2[,mean_col],1, sd , na.rm = TRUE) # same result as before.
+
+View(new2)
+
+#### CALCULATION OF TIME
+new3 <- results_new %>%
+  group_by(Method,Percentage,Type) %>%
+  summarise(Time = sum(Time_total,na.rm=TRUE))%>%
+  ungroup() %>%
+  spread(Type,Time)%>% # it spreads the error through the columns
+  arrange(Method)
+
+colnames(new3)[3:ncol(new3)] <- paste("Time_",colnames(new3)[3:ncol(new3)], sep = "_" )
+# calculate the total sum error of the columns with the mean
+new3$Total_TIME <- rowSums(new3 %>% select(starts_with("Time_")),na.rm = TRUE)
+
+View(new3)
 
 
-View(new)
+new2$Total_TIME_min <- round((rowSums(new3 %>% select(starts_with("Time_")),na.rm = TRUE))/60,digits = 3)
+
+
+Imputation_Matrix <- new2
+
 
 #   
 
