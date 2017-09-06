5b08eb9ca8470d668e4a930780a3ef8cb2c8ff2e
mariekok <marietta.kokla@uef.fi>
2017-09-06 08:58:06 +0300

    hybrid imputation function

--- src/missignessBYgroups.R
+++ src/missignessBYgroups.R
@@ -18,7 +18,7 @@ source(paste0(path,"src/testFuncMIss.R"))
 # use dummy reference data (combination of different metabolomics data)
 reference_data <- as.matrix(read.csv(paste0(path, "data/reference_data.csv")))
 simulated_data <- simulate_data(data=reference_data, nrow=15, ncol=20)
-miss_data <- simulate_missingness(data=simulated_data, mcar=0, mnar=0, mar=0.4)
+miss_data <- simulate_missingness(data=simulated_data, mcar=0, mnar=0, mar=0.3)
 View(miss_data)
 
 
@@ -26,3 +26,46 @@ billy <- detect.miss.MNAR.MAR(miss_data)
 missvar <-data.matrix( billy$MissingVar)
 marmnar <- billy$MAR_MNAR
 mak <- detect.MCAR.MNAR.MAR (MissVAr = missvar,MARMNAR = marmnar, data = miss_data,simulateDATA = simulated_data)
+
+
+listVAR <- check.miss(miss_data)
+df <- miss_data[,listVAR]
+
+
+MNAR <- mak$MNAR
+MCAR <- mak$MCAR
+MAR <- mak$MAR
+newdf <-df[,MNAR$ListMNAR]
+imputation_methods <- c("PPCA", "RF","min","mean","LLS","KNNImpute","BPCA","svdImpute")
+
+nrmse_error <- 0
+nrmse_error1 <- 0
+nrmse_error2 <- 0
+
+if (length(MNAR) > 0 ||length(MNAR) > 0 ||length(MAR) > 0 ){
+  tmp <- impute(df[,MNAR$ListMNAR],'KNNImpute')
+  nrmse_error <- missForest:: nrmse(tmp, df[,MNAR$ListMNAR], simulated_data[,MNAR$ListMNAR])
+  tmp1 <- impute(df[,MCAR$ListMCAR],'BPCA')
+  nrmse_error1 <- missForest:: nrmse(tmp1, df[,MCAR$ListMCAR], simulated_data[,MCAR$ListMCAR])
+  
+  tmp2 <- impute(df[,MAR$ListMAR],'KNNImpute')
+  nrmse_error2 <- missForest:: nrmse(tmp2, df[,MAR$ListMAR], simulated_data[,MAR$ListMAR])
+  
+}
+
+
+hybrid.impute <- function(data,type){
+  
+  switch(type,
+         mnar = impute(data,"min"),
+         mar = impute(data,"mean"),
+         mcar = impute(data,"mean"),
+         marmnar = impute(data,"mean"),
+         marmcar = impute(data,"mean"),
+         mnarmcar = impute(data,"mean"),
+         marmnarmcar = impute(data,"mean")
+  )
+  
+  
+}
+
