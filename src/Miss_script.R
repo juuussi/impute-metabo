d925f4a7d64a9089436b990394852e32ba7f4e3f
mariekok <marietta.kokla@uef.fi>
2017-09-12 13:59:06 +0300

    few changes

--- src/Miss_script.R
+++ src/Miss_script.R
@@ -10,15 +10,15 @@ set.seed(1234)
 # choose path
 path <- "~/projects/impute-metabo/"
 source(paste0(path,"src/functions.R"))
-source(paste0(path,"src/testFuncMIss.R"))
+
 
 # start logging process by creating a logging file
 
 ###################################################################################
 # use dummy reference data (combination of different metabolomics data)
 reference_data <- as.matrix(read.csv(paste0(path, "data/reference_data.csv")))
-simulated_data <- simulate_data(data=reference_data, nrow=20, ncol=50)
-miss_data <- simulate_missingness(data=simulated_data, mcar=0.3, mnar=0, mar=0)
+simulated_data <- simulate_data(data=reference_data, nrow=20, ncol=5)
+miss_data <- simulate_missingness(data=simulated_data, mcar=0.1, mnar=0, mar=0)
 #View(miss_data)
 
 
@@ -34,8 +34,9 @@ listvector
 
 
 
+Methods <- c(rep("min",3),rep("mean",2))
 
-
+test_d<- impute(miss_data,Methods)
 
 
 
