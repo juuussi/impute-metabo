c6c8b5e114dc61e2db87ff43e74037f84355eb0a
mariekok <marietta.kokla@uef.fi>
2017-09-22 18:48:59 +0300

    pipeline of the missing data analysis

--- src/Miss_script.R
+++ src/Miss_script.R
@@ -18,24 +18,9 @@ source(paste0(path,"src/functions.R"))
 ###################################################################################
 # use dummy reference data (combination of different metabolomics data)
 reference_data <- as.matrix(read.csv(paste0(path, "data/reference_data.csv")))
-simulated_data <- simulate_data(data=reference_data, nrow=20, ncol=50
-                                )
-miss_data <- simulate_missingness(data=simulated_data, mcar=0.1, mnar=0.3, mar=0)
+simulated_data <- simulate_data(data=reference_data, nrow=20, ncol=10)
+miss_data <- simulate_missingness(data=simulated_data, mcar=0.3, mnar=0.1, mar=0)
 #View(miss_data)
 
-
-billy <- detect.miss.MNAR.MAR(data.frame(miss_data))
-MissingVar <-data.matrix( billy$MissingVar)
-MAR_MNAR <- billy$MAR_MNAR
-missigness <- detect.MCAR.MNAR.MAR (miss_data,MissingVar,MAR_MNAR)
-
-
-
-listvector <- detect_missingness_type(missigness) 
-
-
-aaa <- select_imputation_method(listvector)
-
-test_d <- impute(miss_data,aaa)
-
+imputed_data <-Run.miss.data(miss_data)
 
